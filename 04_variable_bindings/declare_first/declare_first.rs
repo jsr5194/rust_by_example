0415d2b828d5180825e921975a1861e74396ec27
Jared Rittle <jsr5194@gmail.com>
2020-07-26 14:30:54 -0400

    adding section 4

---
+++ 04_variable_bindings/declare_first/declare_first.rs
@@ -0,0 +1,23 @@
+fn main() {
+	// Declare a variable binding
+	let a_binding;
+
+	{
+		let x = 2;
+
+		// initialize the binding
+		a_binding = x * x;
+	}
+
+	println!("a binding: {}", a_binding);
+
+	let another_binding;
+
+	// Error! Use of uninitialized binding
+	// println!("another binding: {}", another_binding);
+	// FIXME ^ comment out this line
+	
+	another_binding = 1;
+
+	println!("another binding: {}", another_binding);
+}
